4616a735069aeb1082b6499a666657b4a60163de
DinhHarrison <doandinhanh307@gmail.com>
2025-09-23 14:10:56 +0700

    testing

---
+++ iplayedthesegamesbefore.py
@@ -0,0 +1,17 @@
+print("iplayedthesegamesbefore")
+#print("hello "+input())
+num=1/5
+constfruits=("apple","banana")
+dic={"age":0,"id":"0"}
+print(dic)
+print(type(dic)==dict)
+print(type(constfruits)==list)
+print("number "+str(dic["age"])+" "+str(dic["id"]))
+print(f"number {num:10.2f}") #10 allotted space, 2 after decimal,float
+print(f"number {'<':4}{num:4}{'>':4}")
+print(f"number {num:10.2f}")
+name=input("Enter your name: ")
+year=int(input("Enter your year: "))
+print(f"{name} is a valiant knight, born in the year {year}. One morning,"
+      f" {name} woke up... "
+      f"Only {name} could save the village.")
