ad31cf7cc0943af6259bb01d79cff5ea36d88818
DLog <doandinhanh307@gmail.com>
2025-11-17 15:47:46 +0700

    late correction

--- assignments/as6/e6.py
+++ assignments/as6/e6.py
@@ -1,7 +1,7 @@
 def unit_price(p_diameter, p_price):
     r=p_diameter/2
     area=3.14159*r*r
-    return p_price/(area/10/10) # price per square meter
+    return p_price/(area/100/100) # price per square meter
 def main_program():
     d1,p1= [float(i) for i in input("Enter diameter (cm) and price of first pizza separated by space: ").split()]
     d2,p2= [float(i) for i in input("Enter diameter (cm) and price of second pizza separated by space: ").split()]
